1c0f0920356ae7c9c780ca37ed00906f10efccac
JagadeshAnbu <tajagadesh@gmail.com>
2024-03-01 10:09:18 +0530

    third commid

---
+++ backend/EcomAPI/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcomAPI.Models
+{
+
+    // Categories Model
+    public class Category
+    {
+        public int CategoryId { get; set; }
+
+        [Required]
+        public string? CategoryName { get; set; }
+    }
+}
+
